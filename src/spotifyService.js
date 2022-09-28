2b3c7b6ef8afd7063ac801fdce7de55410670f46
Aliaksandra Kozlovskaya <KOZLOVSKAYA.AN@MAIL.RU>
2022-09-28 21:49:26 +0300

    redirect url

--- src/spotifyService.js
+++ src/spotifyService.js
@@ -1,6 +1,6 @@
 
 const clientId = '56cc4ab9abb849c2b30968f0b315297e';
-const redirectUrl = 'http://localhost:3000/';
+const redirectUrl = 'https://gregarious-crostata-14f281.netlify.app/api/auth/callback/spotify';
 const apiUrl = 'https://accounts.spotify.com/authorize';
 const scopes = [
    "user-read-currently-playing",
